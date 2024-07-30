bd6abe8309529724bb1e0617e7d006dbed6e6cc5
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-30 16:31:19 -0700

    Add 'new task' button to homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import inboxLoad from './inbox.js';
+import inputModal from './input.js';
+import './style.css';
 
 inboxLoad();
+
+const content = document.getElementById('content');
+const addTask = document.createElement('button');
+
+addTask.setAttribute('id', 'add');
+addTask.textContent = '+';
+
+addTask.addEventListener('click', () => inputModal());
+
+content.appendChild(addTask);
