886703879a898429fef81b4af0cb8360cbfaf08f
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-07-30 16:35:40 -0700

    Minor changes

--- src/inbox.js
+++ src/inbox.js
@@ -1,5 +1,14 @@
 const inboxLoad = () => {
 
+  const content = document.getElementById('content');
+  const main = document.createElement('div');
+
+  main.classList.add('main');
+
+  main.textContent = 'Hello ToDo!';
+
+  content.appendChild(main);
+
 };
 
 export default inboxLoad;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'To-Do',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
