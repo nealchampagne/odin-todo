4cf6467e85f0da2d45fbebdf3dce7b553c189362
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-01 18:16:28 -0700

    Move the "new todo" button into the proper module

--- src/inbox.js
+++ src/inbox.js
@@ -1,14 +1,20 @@
+import inputModal from "./input";
+
 const inboxLoad = () => {
 
   const content = document.getElementById('content');
   const main = document.createElement('div');
+  const addTask = document.createElement('button');
 
   main.classList.add('main');
-
   main.textContent = 'Hello ToDo!';
+  addTask.setAttribute('id', 'add');
+  addTask.textContent = '+';
+
+  addTask.addEventListener('click', () => inputModal());
 
   content.appendChild(main);
-
+  content.appendChild(addTask);
 };
 
 export default inboxLoad;
