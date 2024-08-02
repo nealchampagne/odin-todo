35705b05a01303cf78a505959d49e396783bee6f
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-01 18:15:48 -0700

    Stand up localStorage capability

--- src/initializestorage.js
+++ src/initializestorage.js
@@ -1,11 +1,15 @@
 const initializeStorage = () => {
 
-  const projects = [];
-  const tasks = [];
-
-  localStorage.setItem('projects', JSON.stringify(projects));
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-
+  /** Check if a projects and/or tasks array exists in localStorage.
+   *  If either doesn't exist, create an empty array */
+  if (!localStorage.getItem('projects')) {
+    const projects = [];
+    localStorage.setItem('projects', JSON.stringify(projects));
+  }
+  if (!localStorage.getItem('tasks')) {
+    const tasks = [];
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 }
 
 export default initializeStorage;
--- src/writestorage.js
+++ src/writestorage.js
@@ -1,7 +1,42 @@
-const writeToStorage = (obj) => {
+const writeToStorage = obj => {
+  
   if (obj.type === 'project') {
+
+    // Grab project array from localStorage
     const storedProjects = JSON.parse(localStorage.getItem("projects"));
-    storedProjects.push(obj);
+
+    // If a project with that id already exists, overwrite it
+    storedProjects.forEach(element => {
+      if (element.id === obj.id) {
+        storedProjects.splice(storedProjects.indexOf(element), 1, obj);
+      }
+    });
+
+    // Now, if that project still doesn't exist in the array, push it in
+    if (!storedProjects.includes(obj)) {storedProjects.push(obj)};
+
+    // Write the updated array back to localStorage
+    localStorage.setItem("projects", (JSON.stringify(storedProjects)));
+
+  };
+  
+  if (obj.type === 'task') {
+
+    // Grab task array from localStorage
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    // If a task with that id already exists, overwrite it
+    storedTasks.forEach(element => {
+      if (element.id === obj.id) {
+        storedTasks.splice(storedTasks.indexOf(element), 1, obj);
+      }
+    });
+
+    // Now, if that task still doesn't exist in the array, push it in
+    if (!storedTasks.includes(obj)) {storedTasks.push(obj)};
+
+    // Write the updated array back to localStorage
+    localStorage.setItem("tasks", (JSON.stringify(storedTasks)));
   }
 };
 
