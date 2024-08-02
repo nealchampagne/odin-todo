0b27c32477c3140075eaeb1d3419f44b5807c024
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-01 18:17:01 -0700

    Wire input form to submit object to localStorage

--- src/input.js
+++ src/input.js
@@ -1,7 +1,10 @@
 import setAttributes from './setattributes.js';
+import { v4 as uuidv4 } from 'uuid';
+import writeToStorage from './writestorage.js';
 
 const inputModal = () => {
 
+  // Create DOM elements for modal input form
   const content = document.getElementById('content');
   const modal = document.createElement('div');
   const close = document.createElement('button');
@@ -23,29 +26,46 @@ const inputModal = () => {
   const prio2 = document.createElement('option');
   const prio3 = document.createElement('option');
   const prio4 = document.createElement('option');
+  
+  // TO IMPLEMENT (PARENT PROJECT FOR TASKS)
+
   const submit = document.createElement('button');
 
-  // 
+  // Set attributes on modal elements
   setAttributes(modal, {'class': 'modal'});
+
+  // Close button
   close.classList.add('closebtn');
   close.textContent = '×';
+
+  // Form
   form.setAttribute('id', 'taskform');
+
+  // Type field
   setAttributes(type, {'class': 'select', 'id': 'type', 'name': 'type'});
   typeLabel.setAttribute('for', 'type');
   typeLabel.textContent = 'Type';
-  setAttributes(type1, {'value': 'project'});
+  setAttributes(type1, {'value': 'task'});
   type1.textContent = 'Task';
-  setAttributes(type2, {'value': 'task'});
+  setAttributes(type2, {'value': 'project'});
   type2.textContent = 'Project';
+
+  // Name field
   setAttributes(name, {'class': 'input', 'id': 'name', 'name': 'name', 'type': 'text'});
   nameLabel.setAttribute('for', 'name');
   nameLabel.textContent = 'Name';
+
+  // Due date
   setAttributes(dueDate, {'class': 'input', 'id': 'duedate', 'name': 'duedate', 'type': 'date'});
   dueLabel.setAttribute('for', 'duedate');
   dueLabel.textContent = 'Due date';
+
+  // Description
   setAttributes(desc, {'class': 'textarea', 'id': 'desc', 'name': 'desc', 'rows': '4', 'cols': '30', 'overflow-y': 'scroll'});
   descLabel.setAttribute('for', 'desc');
   descLabel.textContent = 'Description';
+
+  // Priority
   setAttributes(priority, {'class': 'select', 'id': 'priority', 'name': 'priority'});
   prioLabel.setAttribute('for', 'priority');
   prioLabel.textContent = 'priority';
@@ -55,9 +75,12 @@ const inputModal = () => {
   prio2.textContent = '2';
   prio3.textContent = '3';
   prio4.textContent = '4';
-  setAttributes(submit, {'class': 'submitbtn', 'type': 'submit', 'form': 'taskform'});
+
+  // Submit button
+  setAttributes(submit, {'class': 'submitbtn'});
   submit.textContent = 'Submit';
 
+  // Append elements to the DOM
   content.appendChild(modal);
   modal.appendChild(close);
   modal.appendChild(form);
@@ -78,24 +101,41 @@ const inputModal = () => {
   priority.appendChild(prio2);
   priority.appendChild(prio3);
   priority.appendChild(prio4);
-  form.appendChild(submit);
+  modal.appendChild(submit);
 
+  // Close the modal and reset the form
   close.addEventListener('click', () => {
     form.reset();
     modal.style.display = 'none';
   })
 
-  submit.addEventListener('submit', (event) => {
-    const newTask = {}
-    if (type.value = 'project') {
-      newTask.name = name.value;
-      newTask.dueDate = dueDate.value;
-      newTask.description = desc.value;
-      newTask.priority = priority.value;
+  // Submit the To-do, close the modal, and reset the form
+  submit.addEventListener('click', () => {
+    const newTodo = {};
+    if (type.value === 'project') {
+      newTodo.name = name.value;
+      newTodo.id = uuidv4();
+      newTodo.type = type.value;
+      newTodo.dueDate = dueDate.value;
+      newTodo.description = desc.value;
+      newTodo.priority = priority.value;
+
+    } else if (type.value === 'task') {
+      newTodo.name = name.value;
+      newTodo.id = uuidv4();
+      newTodo.type = type.value;
+      newTodo.dueDate = dueDate.value;
+      newTodo.description = desc.value;
+      newTodo.priority = priority.value;
+      newTodo.subTasks = [];
+      // Get parent project by uuid
     }
-    taskform.reset();
+    writeToStorage(newTodo);
+    // !!TEST!!
+    console.log(newTodo);
+
+    form.reset();
     modal.style.display = 'none';
-    event.preventDefault();
   })
 }
 
