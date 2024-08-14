fd7152393ae5fb4723f105998317cffcd39ca873
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-14 16:04:30 -0700

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import clearContent from './clearcontent.js';
-import inboxLoad from './inbox.js';
-import initializeStorage from './initializestorage.js';
 import './style.css';
-
-inboxLoad();
-initializeStorage();
+import initializeStorage from './initializestorage.js';
+import clearChildren from './clearchildren.js';
+import inboxLoad from './inbox.js';
+import projectsLoad from './projects.js';
+import clearAddButton from './clearaddbutton.js';
 
 const content = document.getElementById('content');
-const inbox = document.getElementById('inbox');
 
-inbox.addEventListener('click', () => {
-  clearContent();
+const inboxBtn = document.getElementById('inbox');
+const projectsBtn = document.getElementById('projects');
+
+initializeStorage();
+inboxLoad();
+
+inboxBtn.addEventListener('click', () => {
+  clearChildren(content);
+  clearAddButton();
   inboxLoad();
+});
+
+projectsBtn.addEventListener('click', () => {
+  clearChildren(content);
+  clearAddButton();
+  projectsLoad();
 });
