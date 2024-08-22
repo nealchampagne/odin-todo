ac81c310abb33c2a55697424bc0c80b28cfcd10a
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 10:56:50 -0700

    Add a function to keep listeners from stacking
    Otherwise listeners would stack on add button

--- src/clearaddbutton.js
+++ src/clearaddbutton.js
@@ -1,5 +1,9 @@
+// Keep event listeners from stacking on add button when pages load
 const clearAddButton = () => {
+
   const addContainer = document.querySelector('.addcontainer');
+
+  // Clone the Add Button container to clear event listeners
   const newAddContainer = addContainer.cloneNode(true);
   addContainer.parentNode.replaceChild(newAddContainer, addContainer);
 }
