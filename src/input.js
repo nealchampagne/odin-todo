ce1fa4d41afc7fb124bf6e2e27078bad4e2a2166
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 11:23:49 -0700

    Comment and polish input.js file

--- src/input.js
+++ src/input.js
@@ -1,14 +1,16 @@
 import setAttributes from './setattributes.js';
 import { v4 as uuidv4 } from 'uuid';
+import { format } from 'date-fns';
 import writeToStorage from './writestorage.js';
 import clearChildren from './clearchildren.js';
 import moment from 'moment/moment.js';
-import clearAddButton from "./clearaddbutton";
+import sort from './sort.js';
 
-const inputForm = (type, func, id = null) => {
+const inputForm = (type, func, id = null, node = null, editName = null, editParent = null, editDate = null, editDesc = null, editPrio = null) => {
 
   // Create DOM elements for input input form
   const container = document.querySelector('.container');
+  const content = document.getElementById('content');
   const addContainer = document.querySelector('.addcontainer');
   const input = document.createElement('div');
   const inputTop = document.createElement('div');
@@ -47,8 +49,11 @@ const inputForm = (type, func, id = null) => {
 
   inputTop.classList.add('inputtop');
   formLabel.classList.add('formlabel');
-  formLabel.textContent = `New ` + type.charAt(0).toUpperCase() + type.slice(1);
-
+  if (editName || editPrio) {
+    formLabel.textContent = `Edit ` + type.charAt(0).toUpperCase() + type.slice(1);
+  } else {
+    formLabel.textContent = `New ` + type.charAt(0).toUpperCase() + type.slice(1);
+  } 
   // Close button
   close.classList.add('closebtn');
   close.textContent = '×';
@@ -56,6 +61,9 @@ const inputForm = (type, func, id = null) => {
   // Name field
   setAttributes(name, {'class': 'nameinput', 'id': 'name', 'name': 'name', 
     'type': 'text', 'hidden': 'true'});
+  if (editName) {
+    name.setAttribute('value', editName);
+  };
 
   setAttributes(nameLabel, {'for': 'name', 'hidden': 'true'});
   nameLabel.textContent = 'Name';
@@ -72,6 +80,9 @@ const inputForm = (type, func, id = null) => {
   // Due date
   setAttributes(dueDate, {'class': 'dateinput', 'id': 'duedate', 'name': 'duedate', 
     'type': 'date' , 'hidden': 'true'});
+  if (editDate && editDate !== 'None') {
+    dueDate.setAttribute('value', format(editDate, 'yyyy-MM-dd'));
+  }
 
   setAttributes(dueLabel, {'for': 'duedate', 'hidden': 'true'});
   dueLabel.textContent = 'Due date';
@@ -79,6 +90,14 @@ const inputForm = (type, func, id = null) => {
   // Description
   setAttributes(desc, {'class': 'textarea', 'id': 'desc', 'name': 'desc',
     'rows': '4', 'cols': '45', 'overflow-y': 'scroll', 'hidden': 'true'});
+  
+  if (editDesc) {
+    if (editDesc === 'None') {
+      desc.textContent = '';
+    } else {
+      desc.textContent = editDesc;
+    }
+  }
 
   setAttributes(descLabel, {'for': 'desc', 'hidden': 'true'});
   descLabel.textContent = 'Description';
@@ -95,7 +114,9 @@ const inputForm = (type, func, id = null) => {
   prio1.textContent = 'High';
   prio1.setAttribute('value', 'high');
   prio2.textContent = 'Medium';
-  setAttributes(prio2, {'selected': 'true', 'value': 'medium'});
+  if (!editPrio) {
+    setAttributes(prio2, {'selected': 'true', 'value': 'medium'});
+  }
   prio3.textContent = 'Low';
   prio3.setAttribute('value', 'low');
   prio4.textContent = 'Lowest';
@@ -104,13 +125,24 @@ const inputForm = (type, func, id = null) => {
   // Submit button
   setAttributes(submit, {'class': 'submitbtn', 'hidden': 'true'});
 
-  submit.textContent = `Submit ${type}`;
+  if (node) {
+    submit.textContent = 'Save changes';
+  } else {
+    submit.textContent = `Submit ${type}`;
+  };
 
   // Append elements to the DOM
   const populateInput = () => {
 
-    container.insertBefore(input, addContainer);
-
+    // If editing a task, insert in that tasks spot in the DOM
+    if (node) {
+      content.insertBefore(input, node);
+      node.style.display = 'none';
+    
+    // Otherwise, append after all existing todo objects
+    } else {
+      content.appendChild(input);
+    };
     input.appendChild(inputTop);
 
     inputTop.appendChild(formLabel);
@@ -129,6 +161,14 @@ const inputForm = (type, func, id = null) => {
 
     form.appendChild(prioLabel);
     form.appendChild(priority);
+
+    // If editing a task, default to the current priority
+    if (editPrio) {
+      const oldPrio = document.createElement('option');
+      setAttributes(oldPrio, {'selected': 'true', 'hidden': 'true', 'disabled': 'true'});
+      oldPrio.textContent = editPrio.charAt(0).toUpperCase() + editPrio.slice(1);
+      priority.appendChild(oldPrio);
+    }
     priority.appendChild(prio0);
     priority.appendChild(prio1);
     priority.appendChild(prio2);
@@ -137,22 +177,39 @@ const inputForm = (type, func, id = null) => {
 
     form.appendChild(parentLabel);
     form.appendChild(parentProject);
+
     clearChildren(parentProject);
 
     const storedProjects = JSON.parse(localStorage.getItem("projects"));
 
-    if (id) {
+    // If adding a task on a project page, only show that project as an option
+    if (id && !node) {
       const fixedProj = storedProjects.find(obj => obj.id === id);
 
-      const projectOption = document.createElement('option');
-      projectOption.setAttribute('value', fixedProj.id);
+      const projectOption = document.createElement('div');
       projectOption.textContent = fixedProj.name;
+      projectOption.setAttribute('value', id);
 
       parentProject.appendChild(projectOption);
+    
     } else {
+
+      // If editing a task, default to the current parent project
       parentProject.appendChild(noProject);
 
-      storedProjects.forEach(element => {
+      if (editParent && editParent !== 'None') {
+        const oldParentName = document.createElement('option');
+
+        const oldParent = storedProjects.find(obj => obj.id === editParent);
+
+        setAttributes(oldParentName, {'selected': 'true', 'hidden': 'true', 'disabled': 'true', 'value': editParent});
+        
+        oldParentName.textContent = oldParent.name.charAt(0).toUpperCase() + oldParent.name.slice(1);
+
+        parentProject.appendChild(oldParentName);
+      }
+
+      sort(storedProjects).forEach(element => {
 
         const projectOption = document.createElement('option');
 
@@ -165,7 +222,7 @@ const inputForm = (type, func, id = null) => {
 
     input.appendChild(submit);
   }
-
+  // if the input is for a task, show all fields
   if (type === 'task') {
 
     input.removeAttribute('class');
@@ -191,6 +248,8 @@ const inputForm = (type, func, id = null) => {
 
     submit.removeAttribute('hidden');
   }
+
+  // If the input is for a project, only show the name field
   if (type === 'project') {
 
     input.removeAttribute('class');
@@ -219,55 +278,81 @@ const inputForm = (type, func, id = null) => {
 
   populateInput();
 
+  /** On submit button click, generate an object with the current input 
+   *  information and write it to localStorage */
+  const submitTodo = () => {
+
+      const newTodo = {};
+      
+      if (type === 'project') {
+
+        newTodo.name = name.value;
+        
+        // If editing a project, use current id
+        if (id) {
+          newTodo.id = id;
+        } else {
+        
+        // Otherwise, generate new id
+          newTodo.id = uuidv4();
+        };
+        newTodo.type = type;
+  
+      } else if (type === 'task') {
+  
+        newTodo.name = name.value;
+
+        // If editing a task, use current id
+        if (id) {
+          newTodo.id = id;
+        } else {
+        
+        // Otherwise, generate a new id
+          newTodo.id = uuidv4();
+        }
+        newTodo.parentId = parentProject.value;
+        newTodo.type = type;
+  
+        if (dueDate.value) {
+  
+          newTodo.dueDate = moment(dueDate.value);
+  
+        } else {
+  
+          newTodo.dueDate = 'None';
+        }
+  
+        newTodo.description = desc.value;
+        newTodo.priority = priority.value;
+        newTodo.complete = false;
+      };
+  
+      writeToStorage(newTodo);
+      form.reset();
+      input.remove();
+      if (id && !node) {
+        func(id);
+      } else {
+        func();
+      };
+  };
+
   // Close the input and reset the form
   close.addEventListener('click', () => {
     form.reset();
     input.remove();
+    if (node) {
+      node.style.display = 'grid';
+    }
   });
 
   // Submit the To-do, close the input, and reset the form
-  submit.addEventListener('click', () => {
-    const newTodo = {};
-    
-    if (type === 'project') {
-      newTodo.name = name.value;
-      newTodo.id = uuidv4();
-      newTodo.type = type;
-
-    } else if (type === 'task') {
-
-      newTodo.name = name.value;
-      newTodo.id = uuidv4();
-      newTodo.parentId = parentProject.value;
-      newTodo.type = type;
-
-      if (dueDate.value) {
-
-        newTodo.dueDate = moment(dueDate.value);
-
-      } else {
-
-        newTodo.dueDate = 'None';
-      }
-
-      newTodo.description = desc.value;
-      newTodo.priority = priority.value;
-      newTodo.subTasks = [];
-    };
-
-    writeToStorage(newTodo);
-    form.reset();
-    input.remove();
-    if (id) {
-      func(id);
-    } else {
-      func();
-    };
-  });
+  submit.addEventListener('click', () => submitTodo())
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
   });
+
 };
 
 export default inputForm;
