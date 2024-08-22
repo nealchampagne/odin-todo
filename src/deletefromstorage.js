8e3cdf1d14c1c1505924d4cc316f037f5beef2c6
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 10:54:31 -0700

    Add a function to delete tasks & projects

---
+++ src/deletefromstorage.js
@@ -0,0 +1,47 @@
+// Delete tasks/projects from local storage
+const deleteFromStorage = (type, id , func, projId = null) => {
+
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+  if (type === 'task') {
+
+    // Find task by id and splice it out of the tasks array
+    const targetTask = storedTasks.find(obj => obj.id === id);
+    const targetTaskIndex = storedTasks.indexOf(targetTask);
+    storedTasks.splice(targetTaskIndex, 1);
+    localStorage.setItem('tasks', JSON.stringify(storedTasks));
+
+    /** Call the page function again to reflect changes
+    /* Call the correct project if this occurs on a project page */
+    if (projId) {
+      func(projId);
+    } else {
+      func();
+    }
+  } else if (type === 'project') {
+
+    const storedProjects = JSON.parse(localStorage.getItem('projects'));
+
+    // Find the project and splice it out of the project array
+    const targetProject = storedProjects.find(obj => obj.id === id);
+    const targetProjIndex = storedProjects.indexOf(targetProject);
+
+    storedProjects.splice(targetProjIndex, 1);
+  
+    localStorage.setItem('projects', JSON.stringify(storedProjects));
+
+    // Then go through and make a new tasks array without the project's tasks
+    const trimmedTasks = [];
+
+    storedTasks.forEach(obj => {
+      if (obj.parentId !== id) {
+        trimmedTasks.push(obj);
+      }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(trimmedTasks));
+    func();
+  }
+} 
+
+export default deleteFromStorage;
