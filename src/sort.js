c3ed476e972e3bbd502571e4d1de6e1a78f09d9a
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 11:10:07 -0700

    Comment and polish array sorting module

--- src/sort.js
+++ src/sort.js
@@ -1,23 +1,28 @@
 import { compareAsc, parseJSON } from 'date-fns';
 
+// Sort the given arrays by date or alphabetically with no date
 const sort = arr => {
 
   if (arr) {
     
     const sortedArray = arr.sort((a, b) => {
 
+      // If no date value (projects), sort alphabetically
       if (!a.dueDate) {
 
-        return a.name.toUpperCase().localeCompare(b.name.toUpperCase);
+        return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
 
       };
 
       if (a.dueDate) {
-
+      
+      // If both have a due date of 'None' then sort alphabetically
       if (a.dueDate === 'None' && b.dueDate === 'None') {
 
         return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
 
+      /** If either has a date and the other has 'None', put the one 
+       *  with no date first */
       } else if (a.dueDate === 'None') {
 
         return -1;
@@ -27,6 +32,8 @@ const sort = arr => {
         return 1;
       }
 
+      /** Put the task with the earlier due date first. If the dates
+       *  are equal, sort alphabetically */
       if (compareAsc(parseJSON(a.dueDate), parseJSON(b.dueDate)) !== 0) {
 
         return compareAsc(parseJSON(a.dueDate), parseJSON(b.dueDate));
