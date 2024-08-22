a0d476cf51cd5ae731a600f6f56db04bd060b71f
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 10:56:20 -0700

    Add a modal to confirm deletion

---
+++ src/confirmdelete.js
@@ -0,0 +1,46 @@
+import deleteFromStorage from "./deletefromstorage";
+
+// Show a confirmation modal in case the user clicked delete by accident
+const confirmDelete = (type, id , func, projId = null) => {
+  
+  const container = document.querySelector('.container');
+  const confirmModal = document.createElement('div');
+  const confirmTitle = document.createElement('div');
+  const confirmSubtext = document.createElement('div');
+  const confirmButtons = document.createElement('div');
+  const yesBtn = document.createElement('button');
+  const noBtn = document.createElement('button');
+
+  confirmModal.classList.add('confirmdelete');
+  confirmTitle.classList.add('confirmtitle');
+  confirmSubtext.classList.add('confirmsubtext');
+  confirmButtons.classList.add('confbuttoncontainer');
+  yesBtn.classList.add('confirmbutton');
+  noBtn.classList.add('confirmbutton');
+
+  confirmTitle.textContent = 'Are you sure?'
+  if (type === 'task') {
+    confirmSubtext.textContent = `Deleted tasks cannot be recovered.`
+  } else if (type === 'project') {
+    confirmSubtext.textContent = `This project and all tasks currently
+     assigned to it will be permanently deleted.`
+  }
+
+  yesBtn.textContent = 'Yes';
+  noBtn.textContent = 'No';
+
+  yesBtn.addEventListener('click', () => {
+    confirmModal.remove();
+    deleteFromStorage(type, id, func, projId = null)
+  });
+  noBtn.addEventListener('click', () => confirmModal.remove());
+
+  content.appendChild(confirmModal);
+  confirmModal.appendChild(confirmTitle);
+  confirmModal.appendChild(confirmSubtext);
+  confirmModal.appendChild(confirmButtons);
+  confirmButtons.appendChild(yesBtn);
+  confirmButtons.appendChild(noBtn);
+};
+
+export default confirmDelete;
