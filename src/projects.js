2500b867861b17077c20589295296fb1ba7c978e
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 11:06:02 -0700

    Polish project.js file

--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,9 @@
 import clearChildren from "./clearchildren";
 import clearAddButton from "./clearaddbutton";
 import inputForm from "./input";
-import populateTasks from "./populatetasks";
 import populateProjects from "./populateprojects";
 
+// Load page with all projects
 const projectsLoad = () => {
   
   const content = document.getElementById('content');
@@ -12,21 +12,26 @@ const projectsLoad = () => {
   clearAddButton();
 
   const projectsHeading = document.createElement('div');
+  const projectsTitle = document.createElement('div');
   const addLabel = document.getElementById('addlabel');
   const addContainer = document.querySelector('.addcontainer');
 
   projectsHeading.classList.add('pageheading');
 
-  projectsHeading.textContent = 'Projects';
+  projectsTitle.classList.add('pagetitle');
+  projectsTitle.textContent = 'Projects';
 
   addLabel.textContent = 'Add project'
 
   content.appendChild(projectsHeading);
+  projectsHeading.appendChild(projectsTitle);
   
+  // Call the input function when the add button is clicked
   addContainer.addEventListener('click', () => inputForm('project', projectsLoad));
 
+  // Populate the projects tab with project cards
   if (JSON.parse(localStorage.getItem('projects'))) {
-    populateProjects(content);
+    populateProjects(content, projectsLoad);
   } else {
     alert(`ERROR: Projects array is missing!`)
   };
