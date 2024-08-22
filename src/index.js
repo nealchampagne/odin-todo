c3d30654df04d83a9b87507fda4b160e3a38d3e0
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-08-22 10:58:50 -0700

    Polish index.js file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 import './style.css';
 import initializeStorage from './initializestorage.js';
 import clearChildren from './clearchildren.js';
-import inboxLoad from './inbox.js';
+import homeLoad from './home.js';
 import projectsLoad from './projects.js';
 import clearAddButton from './clearaddbutton.js';
 
+// Set up storage when the user first loads the page
+initializeStorage();
+
 const content = document.getElementById('content');
 
-const inboxBtn = document.getElementById('inbox');
+const homeBtn = document.getElementById('home');
 const projectsBtn = document.getElementById('projects');
 
-initializeStorage();
-inboxLoad();
+// Load initial page
+homeLoad();
 
-inboxBtn.addEventListener('click', () => {
+// Set up listeners for tab switching
+homeBtn.addEventListener('click', () => {
   clearChildren(content);
   clearAddButton();
-  inboxLoad();
+  homeLoad();
 });
 
 projectsBtn.addEventListener('click', () => {
